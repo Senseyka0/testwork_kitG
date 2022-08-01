5c7ebde34a26f3b2d6f383b8f04f124fc696f526
mikhail_rypta <senseika@gmail.com>
2022-08-01 18:01:59 +0300

    test husky

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,19 +6,17 @@ import { ContentLayout } from "../layouts";
 import { Converter, Currencies } from "../pages";
 import { Header } from "./";
 
-const App = () => {
-	return (
-		<>
-			<Header />
+const App = () => (
+	<>
+		<Header />
 
-			<ContentLayout>
-				<Routes>
-					<Route path={ROUTES.CONVERTER} element={<Converter />} />
-					<Route path={ROUTES.CURRENCIES} element={<Currencies />} />
-				</Routes>
-			</ContentLayout>
-		</>
-	);
-};
+		<ContentLayout>
+			<Routes>
+				<Route path={ROUTES.CONVERTER} element={<Converter />} />
+				<Route path={ROUTES.CURRENCIES} element={<Currencies />} />
+			</Routes>
+		</ContentLayout>
+	</>
+);
 
 export default App;
