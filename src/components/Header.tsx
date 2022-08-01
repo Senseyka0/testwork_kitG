f047b0676eda61bde62b4898f5e20576e69ec191
mikhail_rypta <senseika@gmail.com>
2022-08-01 17:59:33 +0300

    test husky

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,23 +3,21 @@ import { Link } from "react-router-dom";
 
 import { ROUTES } from "../constants/routes";
 
-const Header = () => {
-	return (
-		<AppBar component="nav">
-			<Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
-				<h3>KitG</h3>
+const Header = () => (
+	<AppBar component="nav">
+		<Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
+			<h3>KitG</h3>
 
-				<div>
-					<Link className="header-link" to={ROUTES.CONVERTER}>
-						Converter
-					</Link>
-					<Link className="header-link" to={ROUTES.CURRENCIES}>
-						Currencies
-					</Link>
-				</div>
-			</Toolbar>
-		</AppBar>
-	);
-};
+			<div>
+				<Link className="header-link" to={ROUTES.CONVERTER}>
+					Converter
+				</Link>
+				<Link className="header-link" to={ROUTES.CURRENCIES}>
+					Currencies
+				</Link>
+			</div>
+		</Toolbar>
+	</AppBar>
+);
 
 export default Header;
